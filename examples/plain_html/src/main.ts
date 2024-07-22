814b4f8b24a608ec3e2cedae9b3df7d603d180f6
Ian Yeoh <ianyeoh2@gmail.com>
2024-07-22 13:15:43 +1000

    Implemented basic styling on plain-html page

--- examples/plain_html/src/main.ts
+++ examples/plain_html/src/main.ts
@@ -15,10 +15,17 @@ const ds = new DragSelect({
     },
 });
 
-for (let i = 0; i < 8; i++) {
+const numItems = 8;
+for (let i = 0; i < numItems; i++) {
+    /* Create and register the selectable item */
     const newSelectableItem = document.createElement("div");
     newSelectableItem.className = "item";
     selectableArea.appendChild(newSelectableItem);
+    ds.addSelectableItem(newSelectableItem);
 
-    if (i !== 0) ds.addSelectableItem(newSelectableItem);
+    /* Add a numbered tag to the item */
+    const itemIndexText = document.createElement("p");
+    itemIndexText.className = "item-tag";
+    itemIndexText.innerText = String(i);
+    newSelectableItem.appendChild(itemIndexText);
 }
